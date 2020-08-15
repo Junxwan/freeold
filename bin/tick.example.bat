de5908d30ae0d8202936f655cb6fc09d3f506f8e
junx <hugh8845sam@gmail.com>
2020-08-15 15:09:32 +0800

    feat: tick bat

---
+++ bin/tick.example.bat
@@ -0,0 +1,25 @@
+:: 專案venv path
+CALL <project path>\venv\Scripts\activate
+
+:: 保存tick資料path
+set path=<tick data path>
+
+:: 個股代碼xlsx
+set file=<code xlsx>
+
+:: tick日期
+set /p date=date:
+
+:: cmoney api ck
+set /p ck=ck:
+
+:: cmoney cookie session
+set /p session=session:
+
+:: 專案目錄
+cd /d <project path>
+
+:: venv python path
+<project path>\venv\Scripts\python tick.py -ck=%ck% -session=%session% -date=%date% -dir=%path% -file=%file%
+
+pause
