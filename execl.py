dfef20a9336d2517309a201b1e6d92016c6e0f65
junx <hugh8845sam@gmail.com>
2020-08-16 22:39:04 +0800

    feat: 增加execl轉換成json

---
+++ execl.py
@@ -0,0 +1,42 @@
+import argparse
+import logging
+import os
+from datetime import datetime
+
+from xlsx.year import json as year
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log',
+                        datetime.now().strftime("%Y-%m-%d-xlsx.log"))
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=filename)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-output',
+    help='output path',
+    required=True,
+    type=str
+)
+
+parser.add_argument(
+    '-input',
+    help='input xlsx path',
+    required=True,
+    type=str
+)
+
+parser.add_argument(
+    '-model',
+    help='xlsx model',
+    required=True,
+    type=str
+)
+
+args = parser.parse_args()
+
+if args.model == 'year':
+    year.run(args.input, args.output)
