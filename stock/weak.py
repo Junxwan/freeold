e9690859869fc45ed7784647be6e72502d994ca8
junx <hugh8845sam@gmail.com>
2020-08-23 21:19:29 +0800

    feat: 弱勢股增加收盤價小於開盤

--- stock/weak.py
+++ stock/weak.py
@@ -51,6 +51,9 @@ def list(date, stock):
         if increase[0] >= 0:
             continue
 
+        if open[0] <= close[0]:
+            continue
+
         price.append([name, code, open[0], close[0], max[0], min[0], increase[0], amplitude[0], volume[0], diff])
 
     return sorted(price, key=lambda s: s[9], reverse=True)[:110]
